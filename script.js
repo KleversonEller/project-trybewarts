c480e4aa3e197218b1ceee74ef1950773d6d7f21
tgandr <tgandr@gmail.com>
2022-01-11 21:59:07 +0000

    so falta o ultimo

--- script.js
+++ script.js
@@ -16,5 +16,27 @@ window.onload = function (){
         if (evento.target.checked == true) {
             document.querySelector("#submit-btn").disabled = false
         }
-    })   
+    }) 
+
+    let count = document.querySelector("textarea");
+    count.addEventListener("keyup", function(){
+        let caract = document.querySelector("#counter")
+        let qcaract = document.querySelector("textarea").value.length
+        caract.innerHTML = 500 - qcaract
+    })
+
+    let btn2 = document.querySelector("#submit-btn")
+    btn2.addEventListener("click", function (evento){
+        evento.preventDefault()
+        let form = document.querySelector("#evaluation-form")
+        for (let pos of form) {
+            let subst = document.createElement("p")
+            subst.innerHTML = pos.value
+            console.log(subst)
+            form.appendChild(subst)
+            if (pos != "p"){
+                form.remove(pos)
+            }
+        } 
+    })
 }
