cba8d6c79178123f7cde1828b71d9c2e0f899948
Kleverson Eller <kleverson.a16@gmail.com>
2022-01-12 01:16:41 -0300

    Requisito - 21 e aplicando parametros lint no codigo

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
-window.onload = function (){
-    let btnEnviar = document.querySelector("#entrar")
-    btnEnviar.addEventListener("click", function (evento) {
-        evento.preventDefault()
-        let email = document.querySelector("[name=email]")
-        let password = document.querySelector("[name=password]")
-        if (email.value == "tryber@teste.com" && password.value == "123456") {
-            alert("Olá, Tryber!")
-        } else {
-            alert("Email ou senha inválidos.")
-        }
-    })
-    document.querySelector("#submit-btn").disabled = true
-    let confirmed = document.querySelector("#agreement")
-    confirmed.addEventListener("click", function(evento){
-        if (evento.target.checked == true) {
-            document.querySelector("#submit-btn").disabled = false
-        }
-    }) 
+const btnEnviar = document.querySelector('#entrar');
+btnEnviar.addEventListener('click', (evento) => {
+  evento.preventDefault();
+  const email = document.querySelector('[name=email]');
+  const password = document.querySelector('[name=password]');
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+const btn1 = document.querySelector('#submit-btn');
+btn1.disabled = true;
+const confirmed = document.querySelector('#agreement');
+confirmed.addEventListener('click', (evento) => {
+  if (evento.target.checked === true) {
+    btn1.disabled = false;
+  }
+});
 
-    let count = document.querySelector("textarea");
-    count.addEventListener("keyup", function(){
-        let caract = document.querySelector("#counter")
-        let qcaract = document.querySelector("textarea").value.length
-        caract.innerHTML = 500 - qcaract
-    })
+const count = document.querySelector('textarea');
+count.addEventListener('keyup', () => {
+  const caract = document.querySelector('#counter');
+  const qcaract = document.querySelector('textarea').value.length;
+  caract.innerHTML = 500 - qcaract;
+});
 
-    let btn2 = document.querySelector("#submit-btn")
-    btn2.addEventListener("click", function (evento){
-        evento.preventDefault()
-        let form = document.querySelector("#evaluation-form")
-        for (let pos of form) {
-            let subst = document.createElement("p")
-            subst.innerHTML = pos.value
-            console.log(subst)
-            form.appendChild(subst)
-            if (pos != "p"){
-                form.remove(pos)
-            }
-        } 
-    })
+function selecionados() {
+  const materiasSelecionadas = [];
+  const materias = document.querySelectorAll('.family input[type=checkbox]:checked');
+  for (let index = 0; index < materias.length; index += 1) {
+    materiasSelecionadas.push(materias[index].value);
+  }
+  return materiasSelecionadas.join(', ');
 }
+
+function valores() {
+  const valor = {
+    Nome: `${document.querySelector('#input-name').value} ${
+      document.querySelector('#input-lastname').value
+    }`,
+    Email: `${document.querySelector('#input-email').value}`,
+    Casa: `${document.querySelector('#house').value}`,
+    Família: `${document.querySelector('[name=family]:checked').value}`,
+    Matérias: selecionados(),
+    Avaliação: `${document.querySelector('[name=rate]:checked').value}`,
+    Observações: `${document.querySelector('#textarea').value}`,
+  };
+  return valor;
+}
+const btn2 = document.querySelector('#submit-btn');
+const form2 = document.querySelector('#evaluation-form');
+
+btn2.addEventListener('click', (evento) => {
+  evento.preventDefault();
+  const valor = valores();
+  const form = document.querySelectorAll('#evaluation-form div');
+
+  for (let index = 0; index < form.length; index += 1) {
+    form[index].remove();
+  }
+
+  for (let index = 0; index < Object.keys(valor).length; index += 1) {
+    const cria = document.createElement('p');
+    cria.innerHTML = `${Object.keys(valor)[index]}: ${valor[Object.keys(valor)[index]]}`;
+    form2.appendChild(cria);
+  }
+});
